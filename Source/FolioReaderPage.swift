ed9abd11b83fd6147c2b20c91ea335a124503c7c
Alexey Smirnov <alexey@gmail.com>
2024-07-10 12:40:29 +0800

    undo WKWebView bugfix, need to use NSAllowsArbitraryLoads = true instead

--- Source/FolioReaderPage.swift
+++ Source/FolioReaderPage.swift
@@ -142,18 +142,18 @@ open class FolioReaderPage: UICollectionViewCell, WKNavigationDelegate, UIGestur
         )
     }
 
-    /*
+    
     func loadHTMLString(title: String!,htmlContent: String!, baseURL: URL!) {
         // Insert the stored highlights to the HTML
-        var tempHtmlContent = htmlContentWithInsertHighlights(htmlContent)
+        let tempHtmlContent = htmlContentWithInsertHighlights(htmlContent)
         
         // Load the html into the webview
         webView?.alpha = 0
         webView?.loadHTMLString(tempHtmlContent, baseURL: baseURL)
         
     }
-    */
-    
+
+/*
         func loadHTMLString(title: String!,htmlContent: String!, baseURL: URL!) {
             // Insert the stored highlights to the HTML
             let tempHtmlContent = htmlContentWithInsertHighlights(htmlContent)
@@ -178,7 +178,7 @@ open class FolioReaderPage: UICollectionViewCell, WKNavigationDelegate, UIGestur
 
             webView?.loadFileURL(URL(fileURLWithPath: filePath), allowingReadAccessTo:URL(fileURLWithPath: baseURL.path.deletingLastPathComponent))
     }
-
+*/
 
     // MARK: - Highlights
 
